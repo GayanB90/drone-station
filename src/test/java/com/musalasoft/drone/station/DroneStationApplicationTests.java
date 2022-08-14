8214024da24dd5872808cc92c395b7e85ed242cb
GayanB90 <100739219+GayanB90@users.noreply.github.com>
2022-08-14 22:16:29 +0530

    initial commit

---
+++ src/test/java/com/musalasoft/drone/station/DroneStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musalasoft.drone.station;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
